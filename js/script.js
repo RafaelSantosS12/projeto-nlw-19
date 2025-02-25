7a423ff6d1b91c5d091489401592fe03e586f16e
Rafael Santos <rafaelsantossilva1203@gmail.com>
2025-02-25 10:54:37 -0300

    Fim do nlw

--- js/script.js
+++ js/script.js
@@ -37,12 +37,29 @@ const getUser = (userData) => {
 
 const showInvite = (userData) => {
     app.innerHTML = `
-    <input type="text" id="link" value="https://evento.com?ref=${userData.ref}" disabled>
-    <div id="stats">
+
+    <main>
+        <h3>Inscrição Confirmada</h3>
+
+        <p>Convide mais pessoas e concorra a prêmios! <br> Compartilhe o link e acompanhe as inscrições:</p>
+        <div class="input-group">
+            <label for="link">
+                <img src="/assets/images/link.svg" alt="Link icon">
+            </label>
+            <input type="text" id="link" value="https://evento.com?ref=${userData.ref}" disabled>
+        </div>
+    </main>
+
+    <section class="stats">
+        <div id="stats">
         <h4>${getTotalSubscribers(userData)}</h4>
         <p>Inscrições feitas</p>
-    </div>
+        </div>
+    </section>
     `
+
+    app.setAttribute('class', 'page-invite');
+    updateImageLinks();
 }
 
 const saveUser = (userData) => {
@@ -79,17 +96,68 @@ const formAction = () => {
     };
 }
 
+ /*const updateImageLinks = () => {
+    document.querySelectorAll('img').forEach((img) => {
+        if(img.src.includes('githubusercontent')){
+            return;
+        }
+        img.src = `https://github.com/maykbrito/my-public-files/tree/main/nlw-19${img.src}`
+    });
+}
+*/
+
 const startApp = () => {
     const content = `
-    <form id="form">
-        <input type="email" name="email" placeholder="E-mail" required>
-        <input type="text" name="phone" placeholder="Telefone" required>
-        <button type="submit">Confirmar</button>
-    </form>
+    <main>
+        <section class="about">
+            <div class="section-header">
+                <h2>Sobre o evento</h2>
+                <span class="badge">AO VIVO</span>
+            </div>
+
+            <p>Um evento feito por e para pessoas desenvolvedoras apaixonadas por criar soluções inovadoras e compartilhar conhecimento. Vamos mergulhar nas tendências mais recentes em desenvolvimento de software, arquitetura de sistemas e tecnologias emergentes, com palestras, workshops e hackathons.
+            <br><br> Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito </p>
+        </section>
+
+        <section class="registration">
+            <h2>Inscrição</h2>
+
+            <form id="form">
+                <div class="input-wrapper">
+                    <div class="input-group">
+                        <label for="email">
+                            <img src="/assets/images/mail.svg" alt="Email icon">
+                        </label>
+                        <input type="email" name="email" id="email" placeholder="E-mail">
+                    </div>
+
+                    <div class="input-group">
+                        <label for="phone">
+                            <img src="/assets/images/phone.svg" alt="phone icon">
+                        </label>
+                        <input type="phone" name="phone" id="phone" placeholder="Telefone">
+                    </div>
+                </div>
+
+                <button>
+                    Confirmar
+                    <img src="/assets/images/arrow.svg" alt="Arrow rigth">
+                </button>
+            </form>
+        </section>
+    </main>
     `;
 
     app.innerHTML = content;
+    app.setAttribute('class', 'page-statj');
     formAction();
 }
 
-startApp();
+const header = document.querySelector("header").onclick= startApp;
+
+startApp()
+/* showInvite({
+    email: 'test@test.com',
+    phone: '9999999999',
+    ref: '100'
+}); */
